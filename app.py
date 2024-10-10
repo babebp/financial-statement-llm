8e6a307a82f66ef8b23049431ef2e948c016a083
babebp <tpacharawut@hotmail.com>
2024-10-11 04:21:56 +0700

    feat: add memory across rerun both message and pdf file context

--- app.py
+++ app.py
@@ -10,46 +10,72 @@ import chainlit as cl
 import dotenv
 from langchain.schema.runnable.config import RunnableConfig
 from langchain.chains import conversational_retrieval
+from langchain.memory import ConversationBufferMemory
+
+from operator import itemgetter
+
+from langchain_community.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableLambda
+from langchain.schema.runnable.config import RunnableConfig
+from langchain.memory import ConversationBufferMemory
+
+from chainlit.types import ThreadDict
+import chainlit as cl
+
 
 dotenv.load_dotenv()
-    
-model = ChatOpenAI(model='gpt-4o-mini', streaming=True)
+
+model = ChatOpenAI(model="gpt-4o-mini", streaming=True)
+
 
 def create_chain(docs, model):
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=5120, chunk_overlap=200)
-    documents = text_splitter.split_documents(docs)
-
-    # Embedding text into vector and store into vector database
+    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
     embedding_model = OpenAIEmbeddings()
 
-    db = FAISS.from_documents(documents=documents, 
-                            embedding=embedding_model)
+    if docs is not None:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=5120, chunk_overlap=200)
+        documents = text_splitter.split_documents(docs)
+
+        db = FAISS.from_documents(documents=documents, embedding=embedding_model)
+        db.save_local("faiss_index")
+    else:
+        
+        db = FAISS.load_local("faiss_index", embedding_model,allow_dangerous_deserialization=True)
 
     # Set type of retriever
     retriever = db.as_retriever()
-    
-    template = """Answer the question based on the following context only:
 
-    {context}
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful chatbot"),
+            MessagesPlaceholder(variable_name="history"),
+            (
+                "human",
+                """Answer the question based on the following context only:
 
-    Question: {question}
-    """
-    
-    prompt = ChatPromptTemplate.from_template(template)
+                {context}
 
+                Question: {question}
+                """,
+            ),
+        ]
+    )
 
     def format_docs(docs):
         return "\n\n".join([d.page_content for d in docs])
 
-
     chain = (
         {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | RunnablePassthrough.assign(
+            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+        )
         | prompt
         | model
         | StrOutputParser()
     )
-        
+
     cl.user_session.set("runnable", chain)
 
 
@@ -59,52 +85,60 @@ async def set_profile():
         cl.ChatProfile(
             name="Assistant",
             markdown_description="I am your helpful assistant, ready to answer your questions.",
-            icon="/public/avatar.png"  # Replace with the actual path to your image
+            icon="/public/avatar.png",  # Replace with the actual path to your image
         )
     ]
-    
-    
+
+
 @cl.password_auth_callback
 def auth_callback(username: str, password: str):
     # Fetch the user matching username from your database
     # and compare the hashed password with the value stored in the database
-    mock_user = {
-        "user_1" : ("user1", "pass"),
-        "user_2" : ("user2", "pass")
-    }
-    # 
+    mock_user = {"user_1": ("user1", "pass"), "user_2": ("user2", "pass")}
+    #
     if (username, password) in mock_user.values():
         return cl.User(
             identifier=username, metadata={"role": "user", "provider": "credentials"}
         )
     else:
         return None
-    
+
 
 @cl.on_chat_start
-async def on_chat_start():    
+async def on_chat_start():
     file = await cl.AskFileMessage(
         content="Please upload a PDF file to begin!", accept=["application/pdf"]
-      ).send()
+    ).send()
     
-    msg = cl.Message(content=f"Processing `{file[0].name}`...")
-    await msg.send()
-    
+    if file:
+        msg = cl.Message(content=f"Processing `{file[0].name}`...")
+        await msg.send()
+
+    cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
     docs = PyPDFLoader(file[0].path).load_and_split()
     create_chain(docs, model)
 
-    
 
 @cl.on_chat_resume
 async def on_chat_resume(thread: ThreadDict):
-    print("The user resumed a previous chat session!")
-    
-    
+    memory = ConversationBufferMemory(return_messages=True)
+    root_messages = [m for m in thread["steps"] if m["parentId"] == None]
+    for message in root_messages:
+        if message["type"] == "user_message":
+            memory.chat_memory.add_user_message(message["output"])
+        else:
+            memory.chat_memory.add_ai_message(message["output"])
+
+    cl.user_session.set("memory", memory)
+
+    create_chain(None, model)
+
+
 @cl.on_message
 async def on_message(message: cl.Message):
-    # memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
+    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
 
-    runnable = cl.user_session.get("runnable")
+    runnable = cl.user_session.get("runnable") # type: Runnable
 
     res = cl.Message(content="")
 
@@ -113,5 +147,5 @@ async def on_message(message: cl.Message):
 
     await res.send()
 
-    # memory.chat_memory.add_user_message(message.content)
-    # memory.chat_memory.add_ai_message(res.content)
+    memory.chat_memory.add_user_message(message.content)
+    memory.chat_memory.add_ai_message(res.content)
